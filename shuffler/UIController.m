c8efb3a0a09a24be93ac5fdc0171768a01390852
Jesse Jones <jesse9jones@gmail.com>
2014-06-27 06:53:34 -0700

    WIP on database management

--- shuffler/UIController.m
+++ shuffler/UIController.m
@@ -47,8 +47,6 @@
 		hash[4], hash[5], hash[6], hash[7],
 		hash[8], hash[9], hash[10], hash[11],
 		hash[12], hash[13], hash[14], hash[15]];
-	[self _insertOrReplace:@"ImagePaths" values:@[path, _hash]];
-	LOG_NORMAL("hash = %s", STR(_hash));
 	
 	// Try to use which ever settings the user last set
 	NSError* error = nil;
@@ -59,7 +57,7 @@
 		   Indexing.hash = '%@' AND Appearance.hash = '%@'", _hash, _hash];
 	
 	NSInteger rating = 0;
-	NSString* tags = @"";
+	NSMutableString* tags = [NSMutableString stringWithString:@""];
 	double scaling = 1.0;
 	NSArray* rows = [_database queryRows:sql error:&error];
 	if (rows)
@@ -71,8 +69,9 @@
 			NSString* field = row[0];
 			rating = [field integerValue];
 
-			field = row[1];
-			tags = [field stringByReplacingOccurrencesOfString:@":" withString:@" • "];
+			tags = [NSMutableString stringWithString:row[1]];
+			[tags deleteCharactersInRange:NSMakeRange(tags.length - 1, 1)];
+			[tags replaceOccurrencesOfString:@":" withString:@" • " options:NSLiteralSearch range:NSMakeRange(0, tags.length)];
 			
 			field = row[2];
 			if ([field localizedCaseInsensitiveCompare:@"inf"] != NSOrderedSame)
@@ -127,7 +126,7 @@
 
 - (IBAction)selectNoneTag:(NSMenuItem*)sender
 {
-	[_tagsLabel setStringValue:@""];
+	[_tagsLabel setStringValue:@"None"];
 	[self _saveSettings];
 }
 
@@ -138,13 +137,18 @@
 
 - (void)_saveSettings
 {
+	ASSERT(_mainWindow.path.length > 0);
+	
 	NSInteger index = _ratingPopup.indexOfSelectedItem;
 	NSString* tags = _tagsLabel.stringValue;
 	tags = [tags stringByReplacingOccurrencesOfString:@" • " withString:@":"];
+	tags = [tags stringByAppendingString:@":"];
 	[self _insertOrReplace:@"Indexing" values:@[_hash, [NSString stringWithFormat:@"%ld", (long)index], tags]];
 
 	double scaling = [self _getScaling];
 	[self _insertOrReplace:@"Appearance" values:@[_hash, @"0", [NSString stringWithFormat:@"%f", scaling]]];
+
+	[self _insertOrReplace:@"ImagePaths" values:@[_mainWindow.path, _hash]];
 }
 
 - (double)_getScaling
@@ -188,7 +192,7 @@
 - (NSString*)_toggleTag:(NSString*)tag
 {
 	NSMutableArray* tags;
-	if (_tagsLabel.stringValue.length > 0)
+	if (_tagsLabel.stringValue.length > 0 && [_tagsLabel.stringValue compare:@"None"] != NSOrderedSame)
 		tags = [[_tagsLabel.stringValue componentsSeparatedByString:@" • "] mutableCopy];
 	else
 		tags = [NSMutableArray new];
@@ -245,17 +249,18 @@
 			created = [database update:@"\
 				CREATE TABLE IF NOT EXISTS Indexing(\
 				   hash TEXT NOT NULL PRIMARY KEY\
-				CONSTRAINT valid_hash_len CHECK(length(hash) >= 8),\
+				      CONSTRAINT valid_hash_len CHECK(length(hash) >= 8),\
 				   rating INTEGER NOT NULL\
-				CONSTRAINT valid_rating CHECK(rating >= 0 AND rating <= 3),\
+				      CONSTRAINT valid_rating CHECK(rating >= 0 AND rating <= 3),\
 				   tags TEXT NOT NULL\
+				      CONSTRAINT valid_tags CHECK(length(tags) = 0 OR substr(tags, -1) = ':')\
 				)" error:&error];
 		
 		if (created)
 			created = [database update:@"\
 				CREATE TABLE IF NOT EXISTS Appearance(\
 				   hash TEXT NOT NULL PRIMARY KEY\
-				CONSTRAINT valid_hash_len CHECK\(length(hash) >= 8),\
+				      CONSTRAINT valid_hash_len CHECK\(length(hash) >= 8),\
 				   alignment INTEGER NOT NULL,\
 				   scaling INTEGER NOT NULL\
 				)" error:&error];
@@ -264,16 +269,16 @@
 			created = [database update:@"\
 				CREATE TABLE IF NOT EXISTS Directories(\
 				   path TEXT NOT NULL PRIMARY KEY\
-				CONSTRAINT absolute_path CHECK(substr(path, 1, 1) = '/'),\
+				      CONSTRAINT absolute_path CHECK(substr(path, 1, 1) = '/'),\
 				   process_time INTEGER NOT NULL\
-				CONSTRAINT valid_process_time CHECK(process_time > 0)\
+				      CONSTRAINT valid_process_time CHECK(process_time > 0)\
 				)" error:&error];
 		
 		if (created)
 			created = [database update:@"\
 				CREATE TABLE IF NOT EXISTS ImagePaths(\
 				   path TEXT NOT NULL PRIMARY KEY\
-				CONSTRAINT absolute_path CHECK(substr(path, 1, 1) = '/'),\
+				      CONSTRAINT absolute_path CHECK(substr(path, 1, 1) = '/'),\
 				   hash TEXT NOT NULL\
 				)" error:&error];
 		
